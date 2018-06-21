b9b5a348e860a619c0e9a181b6ac2c1dcc494951
MichaelBorrasso <michaelborrasso@gmail.com>
2018-06-20 20:11:28 -0400

    server modified

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {
 
 // Initialize Express
 var app = express();
